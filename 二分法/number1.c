c510060d3629f415c22a02c5ea2dc5837e9fdceb
Elsia-aot <Qq2602539653@outlook.com>
2025-03-26 21:26:56 +0800

    增加了左闭右开区间的写法

---
+++ 二分法/number1.c
@@ -0,0 +1,39 @@
+//用[left.right)的区间完成二分搜索
+#include <stdio.h>
+int search(int *nums,int numsize,int target)//传入数组指针，数组长度和目标值
+{
+    int left=0,right=numsize;
+    while (left<right)
+    {
+         int middle=(left+right)/2;
+         if (nums[middle]>target)
+         {
+            right=middle;
+         }
+         else if (nums[middle]<target)
+         {
+            left=middle+1;
+         }
+         else{
+            return middle;
+         }
+    }
+    return 0;
+}
+int main() {
+    // 测试用例1：目标存在
+    int arr1[] = {-1, 0, 3, 5, 9, 12};           // 有序数组
+    int target1 = 12;                             // 存在的目标值
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);  // 计算数组长度
+    int result1 = search(arr1, size1, target1);  // 调用查找函数
+    printf("text1: target %d the undernumber is %d\n", target1, result1);
+
+    // 测试用例2：目标不存在
+    int arr2[] = {2, 4, 6, 8, 10};
+    int target2 = 5;
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+    int result2 = search(arr2, size2, target2);
+    printf("text2: target %d the undernumber is %d\n", target2, result2);
+
+    return 0;
+}
